e4bd1a8f9f4417be845634485c619350f8eb944a
ansanloms <ansanloms@users.noreply.github.com>
2022-05-08 18:18:13 +0900

    Add watch.

--- bin/build.js
+++ bin/build.js
@@ -2,12 +2,21 @@ const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 const path = require("path");
 
+const watch = process.argv[2] === "watch";
+
 build({
   entryPoints: [path.join(__dirname, "../src/nord.scss")],
   outdir: path.join(__dirname, "../dist"),
   bundle: true,
   minify: true,
+  watch: watch,
+  loader: {
+    ".png": "dataurl",
+  },
   plugins: [sassPlugin()],
 })
-  .then(() => console.log("⚡ Build complete! ⚡"))
-  .catch(() => process.exit(1));
+  .then(() => console.log(watch ? "watching..." : "Build complete!"))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
