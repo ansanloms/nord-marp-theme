d3b4c67bd5ba1800240763ddad9a3896cd21939f
ansanloms <ansanloms@users.noreply.github.com>
2023-10-18 00:00:50 +0900

    upgrade

--- bin/build.js
+++ bin/build.js
@@ -2,20 +2,17 @@ const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 const path = require("path");
 
-const watch = process.argv[2] === "watch";
-
 build({
   entryPoints: [path.join(__dirname, "../src/nord.scss")],
   outdir: path.join(__dirname, "../dist"),
   bundle: true,
   minify: true,
-  watch: watch,
   loader: {
     ".png": "dataurl",
   },
   plugins: [sassPlugin()],
 })
-  .then(() => console.log(watch ? "watching..." : "Build complete!"))
+  .then(() => console.log("Build complete!"))
   .catch((err) => {
     console.error(err);
     process.exit(1);
